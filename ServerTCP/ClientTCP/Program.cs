4829c493aa3577c058834216edaa2a0ad9a7c1c8
Ulyana <ulianabober@gmail.com>
2024-02-11 15:24:20 +0300

    Add client and server

---
+++ ServerTCP/ClientTCP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Enter message: ");
+            var message = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(message);
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data);
+
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+
+            do
+            {
+                size = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            }
+            while (tcpSocket.Available > 0);
+
+            Console.WriteLine(answer.ToString());
+
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+        }
+    }
+}
